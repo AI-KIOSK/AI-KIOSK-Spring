d8f7b2f01890e8649027f6c67f965fbf592b90fc
kkongchi <smallbean99s@gmail.com>
2023-05-03 13:37:46 +0900

    init project

---
+++ src/main/java/com/hongik/AIKIOSK/AikioskApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.AIKIOSK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongik/AIKIOSK/AikioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.AIKIOSK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
